89ff58c9db6f63ecd05be103c0f3ed4d95537970
Jimmy Källhagen <TheEnchantedPotato@proton.me>
2026-03-09 19:50:40 +0100

    Update term-run.sh

--- term-run/term-run.sh
+++ term-run/term-run.sh
@@ -3,7 +3,7 @@
  # SPDX-License-Identifier: GPL-3.0-or-later                         #
  # Copyright (c) 2025 Jimmy Källhagen                                #
  # Part of Yggdrasil - Nordix desktop environment                    #
- # Nordix and Yggdrasil are registered trademarks of Jimmy Källhagen # 
+ # Nordix and Yggdrasil are trademarks of Jimmy Källhagen # 
 ##===================================================================##
 
 # term-run - Auto-detects and runs terminal commands
