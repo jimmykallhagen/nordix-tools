65976c04c34b4b3ca233ebe9552d10c0f9f8aa69
Jimmy Källhagen <TheEnchantedPotato@proton.me>
2026-04-25 12:24:53 +0000

    Update nordix-sock-call.c

--- nordix-sock-call.c
+++ nordix-sock-call.c
@@ -1,10 +1,9 @@
-/*============================================================================*
- * SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0                      *
- * Nordix license - https://polyformproject.org/licenses/noncommercial/1.0.0  *
- * Copyright (c) 2025 Jimmy Källhagen                                         *
- * Part of Nordix - https://github.com/jimmykallhagen/Nordix                  *
- * Nordix and Yggdrasil are trademarks of Jimmy Källhagen                     *
- *============================================================================*/
+/*============================================================*
+ * SPDX-License-Identifier:  GPL-3.0-or-later                 *
+ * Nordix license - https://www.gnu.org/licenses/gpl-3.0.html *
+ * Copyright (c) 2025 Jimmy Källhagen                         *
+ * Part of Nordix - https://github.com/jimmykallhagen/Nordix  *
+ *============================================================*/
 
 #include <sys/socket.h>
 #include <sys/un.h>
